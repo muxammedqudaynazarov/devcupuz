99afcfe8763c34502acf89ac3c0cb1ccac6a219a
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-04-03 09:16:59 +0500

    Q-1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AttemptController;
+use App\Http\Controllers\UpdateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/check', [AttemptController::class, 'checkCode']);
+
+
+Route::get('/sensordata', [UpdateController::class, 'sensordata']);
+Route::get('/led/{code}/{led_no}/{change}/{status}', [UpdateController::class, 'led_on']);
+Route::get('/led/{code}/{led_no}', [UpdateController::class, 'led_on']);
+Route::get('/get_status', [UpdateController::class, 'get_status']);
